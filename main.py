58a96f8da029d04933a3e49e296a023eed80c821
sola1010nuo <jenny38178843@gmail.com>
2024-06-19 06:23:15 +0800

    修正參數封裝問題

--- main.py
+++ main.py
@@ -255,7 +255,7 @@ def get_rental(id):
     connection = create_connection()
     cursor = connection.cursor()
     cursor.execute(
-        "SELECT * FROM Rental WHERE R_id=%s", (id))
+        "SELECT * FROM Rental WHERE R_id=%s", (id,))
     rental = cursor.fetchone()
     cursor.close()
     connection.close()
