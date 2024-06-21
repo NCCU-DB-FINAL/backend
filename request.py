61b0ea2d395fc7a73d0d43583f5d1163b7cdf38b
Afan Chen <afan890918@gmail.com>
2024-06-21 23:09:53 +0800

    Add files via upload

--- request.py
+++ request.py
@@ -1,8 +1,11 @@
 import json
 import random
+from time import sleep
+
 import requests
 from mysql.connector import connect, Error
 from datetime import datetime
+
 
 def create_connection():
     """建立連接"""
@@ -67,11 +70,11 @@ def post_rental(data, token):
         print(f"Failed to add rental: {response.content}")
 
 
-def fetch_rental_data():
+def fetch_rental_data(page):
     url = 'https://rent.591.com.tw/home/search/rsList'
     payload = {
         'is_format_data': 1,
-        'is_new_list': 3,
+        'is_new_list': page,
         'type': 1,
         'region': 1,
         'section': '1,2,3',
@@ -85,7 +88,7 @@ def fetch_rental_data():
         'Accept-Language': 'zh-TW,zh;q=0.9',
         'Connection': 'keep-alive',
         # TODO: Replace these with your own values
-        'Cookie': 'T591_TOKEN=htl2rt26sutg9pltej6ibt053h; rentPreventFraud=0; webp=1; PHPSESSID=4topmdqudb5tfk5ac4fcehmqdf; urlJumpIp=1; new_rent_list_kind_test=1; newUI=1; tw591__privacy_agree=0; XSRF-TOKEN=eyJpdiI6IkhzVDdubmdDdU5KZnptbTgwc3hoNlE9PSIsInZhbHVlIjoiUmZLeWU0TjgxeUJMZnl2U0o5VWloVHUzMjZjeU43akhkZUMyc1VxTG8zTUQ0cXRLSlJKdFZ6Z1hNMnFZbDBLSmxVVWl4TDB4d05aTkFzendIV3FVVWl0M0pUcXFJZ29SK3h6QjVXaUdGQ296Ry9qN3E5eUFiSktnMU45Y3B2RHAiLCJtYWMiOiJhY2NkZmZmZGYwYWU4ZTM5ZjE2NWU3OTcwY2JlZGMwMjYwYTJhNTlhNmQ3NzBmMWExOWFlMGQ2MTM4OGE3MWUwIiwidGFnIjoiIn0%3D; 591_new_session=eyJpdiI6InhMOWpIYVJLSy9vYXZUOWR0SitKS1E9PSIsInZhbHVlIjoiZ0ZjZnVwZ3NtQTF4aHB6bGRUQXFrbEtyTnFYT0V5WkYzTGtNQ1dBUjZwZGdUL3I5Si80TTY4SFRkK3phKzJkeS9NaDVVa05tVStsbHBXVm5uM3IxQ3VEOUJKekQyMnQzVW13QnJ3MnZmcjJvRVkwanN1akQzcUlCbmxwNG8xMy8iLCJtYWMiOiIzYjI5MzRjYzgwMTA5OTBlNDlmYzA4YWE3NjQxYTBmZTE5NTk3MzY5ZGE1NTFjNTkwZDQ1Y2RmMjNkNjZmMzFiIiwidGFnIjoiIn0%3D; timeDifference=0',
+        'Cookie': 'T591_TOKEN=htl2rt26sutg9pltej6ibt053h; rentPreventFraud=0; webp=1; PHPSESSID=4topmdqudb5tfk5ac4fcehmqdf; urlJumpIp=1; newUI=1; tw591__privacy_agree=0; new_rent_list_kind_test=0; house_detail_stat=%5B%7B%22type%22%3A%221%22%2C%22resource%22%3A%228%22%2C%22post_id%22%3A%2216857107%22%7D%5D; timeDifference=-1; XSRF-TOKEN=eyJpdiI6InlYOFNSZCtLV1VzOUh4ekdVeUZCQnc9PSIsInZhbHVlIjoiNWQ4SE9YbEZMWTlXOVlsd0xldlBocDBhdTN2dUk2d3c4MnlKNVd1eWl3bDRlYmR1OEVnaWZVOFRNeENjWXVtcXVqeDBVdGJyYm0wYllGZXVaZmF5SjdWOTluNGdyanliTXNaekNWRHR3azF3NWFudHEyZ0g4RXpjV3JhYTRTSUYiLCJtYWMiOiJmNjQ2ZTAzZjllOWI0NWUzMzZmNjAyMWRlNDNlODhlOTI3Y2Q3ZGFlOWIyMTg3MWJjNGEwY2U4ZWU0ZDEzOTI1IiwidGFnIjoiIn0%3D; 591_new_session=eyJpdiI6Ik13WGEwRUdDeUN4b3FoRWpCWlBHWEE9PSIsInZhbHVlIjoiTmErS0JRcThLenpJZHNuUjlKR2pLYWw1WTVGRmhxWHc5UXhSRG1kVHAwMmRVVEpHY3phRnJkZ0dGUk1EdXBrRWJSaUp3VVU3L2lVZytIZ3JONHJQaDVBQkEyU1IvYUtYdTlKZnRBMEZTNFNGUzFscDF4dlNua2pnbVRkOVNCRUMiLCJtYWMiOiIwY2IxMzA1NTIzNDgzNGIxYmFmMWM4MjQyNDMwOTk5MDYyNDAwZWEzOWU0MDViMDk4YmQ1ZTNiYjI3ZDFhYzJmIiwidGFnIjoiIn0%3D',
         'Device': 'pc',
         # TODO: Replace these with your own values
         'Deviceid': 'htl2rt26sutg9pltej6ibt053h',
@@ -100,7 +103,7 @@ def fetch_rental_data():
         'Sec-Gpc': '1',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
         # TODO: Replace these with your own values
-        'X-Csrf-Token': 'hTCq6mdhZFohZhvQIJoBllliiPtSVLJmbojI1rGp',
+        'X-Csrf-Token': 'OtkdSswr0cBHRdFZRJ1inXecyXHCpelMby52xzGB',
         'X-Requested-With': 'XMLHttpRequest'
     }
 
@@ -113,11 +116,6 @@ def fetch_rental_data():
 
 
 def process_and_insert_data(username, phonenum, password):
-    rental_data = fetch_rental_data()
-    print(rental_data)
-    if rental_data is None or not rental_data.get('data', {}).get('topData', []):
-        return
-
     if not register_landlord(username, phonenum, password):
         return
 
@@ -125,37 +123,45 @@ def process_and_insert_data(username, phonenum, password):
     if token is None:
         return
 
-    for rental in rental_data['data']['topData']:
-        address = rental['section_name'] + ' ' + rental['street_name'] + ' ' + rental['title']
-        price = int(rental['price'].replace(',', ''))
-        # type_ = rental['type']
-        type_ = '整層'
-        room_str = rental['room_str']
-        bedroom = int(room_str[0]) if '房' in room_str else 0
-        if '房' in room_str:
-            room_str = room_str.split('房')[1]
-        living_room = int(room_str[0]) if '廳' in room_str else random.randint(0, 2)
-        if '廳' in room_str:
-            room_str = room_str.split('廳')[1]
-        bathroom = int(room_str[0]) if '衛' in room_str else random.randint(0, 3)
-        if '衛' in room_str:
-            room_str = room_str.split('衛')[1]
-        ping = float(rental['area'])
-        rental_term = rental['rent_tag'][0]['name'] if rental['rent_tag'] else '未知'
-        post_date = datetime.now().strftime('%Y-%m-%d')
+    for page in range(6, 20):
+        rental_data = fetch_rental_data(page)
+        print(rental_data)
+        if rental_data is None or not rental_data.get('data', {}).get('topData', []):
+            continue
 
-        data = {
-            'address': address,
-            'price': price,
-            'type': type_,
-            'bedroom': bedroom,
-            'living_room': living_room,
-            'bathroom': bathroom,
-            'ping': ping,
-            'rental_term': rental_term,
-            'post_date': post_date
-        }
-        post_rental(data, token)
+        for rental in rental_data['data']['topData']:
+            address = rental['section_name'] + ' ' + rental['street_name']
+            title = rental['title']
+            price = int(rental['price'].replace(',', ''))
+            # type_ = rental['type']
+            type_ = random.choice(['整層', '套房', '雅房'])
+            room_str = rental['room_str']
+            bedroom = int(room_str[0]) if '房' in room_str else 0
+            if '房' in room_str:
+                room_str = room_str.split('房')[1]
+            living_room = int(room_str[0]) if '廳' in room_str else random.randint(0, 2)
+            if '廳' in room_str:
+                room_str = room_str.split('廳')[1]
+            bathroom = int(room_str[0]) if '衛' in room_str else random.randint(0, 3)
+            if '衛' in room_str:
+                room_str = room_str.split('衛')[1]
+            ping = float(rental['area'])
+            rental_term = rental['rent_tag'][0]['name'] if rental['rent_tag'] else '未知'
+            post_date = datetime.now().strftime('%Y-%m-%d')
+
+            data = {
+                'address': address,
+                'title': title,
+                'price': price,
+                'type': type_,
+                'bedroom': bedroom,
+                'living_room': living_room,
+                'bathroom': bathroom,
+                'ping': ping,
+                'rental_term': rental_term,
+                'post_date': post_date
+            }
+            post_rental(data, token)
 
 
 if __name__ == '__main__':
